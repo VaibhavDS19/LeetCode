e5456102d66567f146a052f428042b9af2d5e725
vaibhav <vaibhavdinakar@gmail.com>
2021-03-21 12:30:50 +0530

    added formatting

--- Rust/0001-1000/001-100/01-10/01.rs
+++ Rust/0001-1000/001-100/01-10/01.rs
@@ -1,15 +1,14 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut h:HashMap<i32, usize> = HashMap::new();
+        let mut h: HashMap<i32, usize> = HashMap::new();
         let mut v = Vec::new();
         for i in 0..nums.len() {
             if !h.contains_key(&nums[i]) {
                 h.insert(target - nums[i], i);
-            }
-            else {
+            } else {
                 v.push(i as i32);
-                v.push( *h.get(&nums[i]).unwrap() as i32 );
+                v.push(*h.get(&nums[i]).unwrap() as i32);
             }
         }
         v
