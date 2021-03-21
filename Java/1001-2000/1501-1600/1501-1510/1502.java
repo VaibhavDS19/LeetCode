0c6b91b0fa2bb457de03b11057c585cc2b29843b
VaibhavDS19 <vaibhavdinakar@gmail.com>
2021-03-21 20:56:20 +0530

    More problems solved

--- Java/1001-2000/1501-1600/1501-1510/1502.java
+++ Java/1001-2000/1501-1600/1501-1510/1502.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
         Arrays.sort(arr);
         int diff = arr[1] - arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] - arr[i - 1] != diff) {
+        for(int i=1;i<arr.length;i++) {
+            if(arr[i] - arr[i-1] !=diff) {
                 return false;
             }
         }
